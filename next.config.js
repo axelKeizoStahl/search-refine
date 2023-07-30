971116fcde1861ee79b3975e64f8fcdb522c346b
axelKeizoStahl <116924011+axelKeizoStahl@users.noreply.github.com>
2023-07-30 01:35:48 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    OPENAI_API_KEY : "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",
-    GOOGLE_API_URL : "https://www.googleapis.com/customsearch/v1",
-    GOOGLE_API_KEY : "AIzaSyB5ovc4GlyJNMbvzQGcXNme0JhGzAQWDW0",
-    CX : "522e3ec0502e94ffe"
-  }
+  output: 'export'
 }
 
 module.exports = nextConfig
