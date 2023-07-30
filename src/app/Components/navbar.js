0d2bb32cdd04c153f238f6aee54a01cff0e20c39
axelKeizoStahl <axelkeizo@gmail.com>
2023-07-30 02:57:09 -0400

    format change

--- src/app/Components/navbar.js
+++
@@ -1,8 +0,0 @@
-import Link from 'next/link'
-
-export default function Navbar() {
-  return (
-    <div className='navbar'>
-      <Link className='homebutton' href="/"><div></div></Link>
-    </div>
-)}
